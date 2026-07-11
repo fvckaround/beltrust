7210d2b561c26ab276ed2dfb8f84ba6e45db2ae3
kingsleyonyekwelu <kimonyekwelu@gmail.com>
2026-07-11 21:10:30 +0100

    Add landing page install app section

--- src/app/(public)/page.js
+++ src/app/(public)/page.js
@@ -1,5 +1,4 @@
 import Hero from "@/components/landing/Hero";
-import InstallSection from "@/components/landing/InstallSection";
 import Features from "@/components/landing/Features";
 import SecuritySection from "@/components/landing/SecuritySection";
 import Testimonials from "@/components/landing/Testimonials";
@@ -9,7 +8,6 @@ export default function HomePage() {
   return (
     <>
       <Hero />
-      <InstallSection />
       <Features />
       <SecuritySection />
       <Testimonials />
